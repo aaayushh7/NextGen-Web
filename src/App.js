ce899f1435c16a630fd87159167c131bcfede962
Ayush <aayushtiwari071@gmail.com>
2023-08-19 19:05:09 +0530

    Template

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
+      <h1>Hello world !</h1>
+    </div>
+  );
+}
+
+export default App;
